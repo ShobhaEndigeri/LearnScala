acd28184714af97d45dca996f6e84ccb83a0a143
Shobha <shobhaendigeri@gmail.com>
2017-09-21 13:29:40 +0530

    Add files via upload
    Collections in Scala

---
+++ collection2.scala
@@ -0,0 +1,18 @@
+package com.sundogsoftware.spark
+
+import scala.collection.mutable.ArrayBuffer
+
+object collection2 {
+  def main(args:Array[String]){
+    val buf = new ArrayBuffer[Int]()
+    buf +=10
+    buf +=20
+    buf +=30
+    buf +=40
+    
+    buf.foreach((x:Int) => print(x+" "))
+    println()
+    println(buf.length)
+    println(buf(1))
+  }
+}
