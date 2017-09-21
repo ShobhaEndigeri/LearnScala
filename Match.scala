b449288c2692067d4576bca4ae3f54e68cb2a8cf
Shobha <shobha.endigeri@nokia.com>
2017-09-21 12:26:52 +0530

    Match

---
+++ Match.scala
@@ -0,0 +1,14 @@
+package com.sundogsoftware.spark
+
+object Match {
+  def main(args: Array[String]){
+    for( a <- args ) a match{
+      case "1" =>
+        println("argument is one");
+      case "2" =>
+        println("argument is two");
+      case x =>
+        println("Unknown");
+    }
+  }
+}
