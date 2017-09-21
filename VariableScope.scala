383ded85e7b9d33f284e3f54353fe2d3ddffdaa8
Shobha <shobhaendigeri@gmail.com>
2017-09-21 15:27:25 +0530

    Variable scope examples

---
+++ VariableScope.scala
@@ -0,0 +1,27 @@
+package com.sundogsoftware.spark.examples
+
+object VariableScope {
+  def main(args:Array[String]) {
+    printA()
+    printB()
+  }
+  
+  def printA() = {
+    val a = 1;
+    {
+      val a = 2;
+      {
+        println(a)
+      }
+    }
+  }
+  
+  def printB() = {
+    val b = 10;
+    {
+      val b = 20;
+      println(b)
+    }
+    println(b);
+  }
+}
