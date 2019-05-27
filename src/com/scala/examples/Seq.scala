66c7ee8e60a321088a68be328c4366074a4be1ff
Shobha <shobhaendigeri@gmail.com>
2019-05-27 17:08:03 +0530

    Update Seq.scala

--- src/com/scala/examples/Seq.scala
+++ src/com/scala/examples/Seq.scala
@@ -1,4 +1,4 @@
-var seq:Seq[Int] = Seq(52,85,1,8,3,2,7)
+        var seq:Seq[Int] = Seq(52,85,1,8,3,2,7)
         seq.foreach((element:Int) => print(element+" "))
         println("\nAccessing element by using index")
         println(seq(2))
